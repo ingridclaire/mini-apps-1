6edbfa08bb56fc418a83d2f1f0865d88645c78d5
Ingrid Carlson <ingridclaire@gmail.com>
2020-04-24 12:36:39 -0700

    added alreadyPlayed object to keep track of toggled boxes

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,13 +1,13 @@
 console.log('I am building a tic tac toe game!');
 
-
-
+//Model
+var boxes = ['topL', 'topM', 'topR', 'midL', 'midM', 'midR', 'bottomL', 'bottomM', 'bottomR'];
+var alreadyPlayed = {topL: false, topM: false, topR: false, midL: false, midM: false, midR: false, bottomL: false, bottomM: false, bottomR: false};
+var previous = '';
 
 
 
 //Controller
-
-var boxes = ['topL', 'topM', 'topR', 'midL', 'midM', 'midR', 'bottomL', 'bottomM', 'bottomR'];
 
 boxes.map(box => {
   return document.getElementById(box).addEventListener("click", () => {
