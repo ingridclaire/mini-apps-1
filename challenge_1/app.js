6805718d74135fc8173584fb423b65514466d68b
Ingrid Carlson <ingridclaire@gmail.com>
2020-04-24 12:23:44 -0700

    add x's to box on click

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,29 +1,17 @@
 console.log('I am building a tic tac toe game!');
 
-document.getElementById("topL").addEventListener("click", () => {
-  console.log('I clicked this box');
-})
-document.getElementById("topM").addEventListener("click", () => {
-  console.log('I clicked this box');
-})
-document.getElementById("topR").addEventListener("click", () => {
-  console.log('I clicked this box');
-})
-document.getElementById("midL").addEventListener("click", () => {
-  console.log('I clicked this box');
-})
-document.getElementById("midM").addEventListener("click", () => {
-  console.log('I clicked this box');
-})
-document.getElementById("midR").addEventListener("click", () => {
-  console.log('I clicked this box');
-})
-document.getElementById("bottomL").addEventListener("click", () => {
-  console.log('I clicked this box');
-})
-document.getElementById("bottomM").addEventListener("click", () => {
-  console.log('I clicked this box');
-})
-document.getElementById("bottomR").addEventListener("click", () => {
-  console.log('I clicked this box');
-})
+
+
+
+
+
+//Controller
+
+var boxes = ['topL', 'topM', 'topR', 'midL', 'midM', 'midR', 'bottomL', 'bottomM', 'bottomR'];
+
+boxes.map(box => {
+  return document.getElementById(box).addEventListener("click", () => {
+    document.getElementById(box).innerHTML = "X";
+  })
+});
+
