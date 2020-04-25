5ff8ad31dc70cc63a9316de00afcdc891be01149
Ingrid Carlson <ingridclaire@gmail.com>
2020-04-24 20:20:32 -0700

    built basic index.html page; serving it up with express.static

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+app.use(express.static('client'));
 const PORT = 2020;
 
+
 app.get('/', (req, res) => {
-  res.send("Hello everybody!!!")
+  console.log('hello everybody!!!!');
+  res.send('Yes it works!!!')
 })
 
 app.listen(PORT, () => console.log(`Express app is listening at http://localhost:${PORT}`))
