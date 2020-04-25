8f6852a520878a6b93a97d9c45301f95c1bffdff
Ingrid Carlson <ingridclaire@gmail.com>
2020-04-24 19:40:52 -0700

    set up bare bones express server; listening on local host

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,10 @@
+const express = require('express');
+const app = express();
+const PORT = 2020;
 
+app.get('/', (req, res) => {
+  res.send("Hello everybody!!!")
+})
+
+app.listen(PORT, () => console.log(`Express app is listening at http://localhost:${PORT}`))
 
